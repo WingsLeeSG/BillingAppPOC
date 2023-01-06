6630e7681804fbb540a8b0c0b92c779dba63f7b6
Wing Kong Lee <wingkong.lee@t-systems.com>
2023-01-06 04:19:22 +0000

    first commit

---
+++ webapp/controller/ServiceSearch.js
@@ -0,0 +1,172 @@
+sap.ui.define([
+	"sap/ui/base/ManagedObject",
+	"sap/m/MessageBox",
+	"./utilities",
+	"sap/ui/core/routing/History"
+], function(ManagedObject, MessageBox, Utilities, History) {
+
+	return ManagedObject.extend("com.sap.build.standard.pocPatientServiceAndInvoice.controller.ServiceSearch", {
+		constructor: function(oView) {
+			this._oView = oView;
+			this._oControl = sap.ui.xmlfragment(oView.getId(), "com.sap.build.standard.pocPatientServiceAndInvoice.view.ServiceSearch", this);
+			this._bInit = false;
+		},
+
+		exit: function() {
+			delete this._oView;
+		},
+
+		getView: function() {
+			return this._oView;
+		},
+
+		getControl: function() {
+			return this._oControl;
+		},
+
+		getOwnerComponent: function() {
+			return this._oView.getController().getOwnerComponent();
+		},
+
+		open: function() {
+			var oView = this._oView;
+			var oControl = this._oControl;
+
+			if (!this._bInit) {
+
+				// Initialize our fragment
+				this.onInit();
+
+				this._bInit = true;
+
+				// connect fragment to the root view of this component (models, lifecycle)
+				oView.addDependent(oControl);
+			}
+
+			var args = Array.prototype.slice.call(arguments);
+			if (oControl.open) {
+				oControl.open.apply(oControl, args);
+			} else if (oControl.openBy) {
+				oControl.openBy.apply(oControl, args);
+			}
+		},
+
+		close: function() {
+			this._oControl.close();
+		},
+
+		setRouter: function(oRouter) {
+			this.oRouter = oRouter;
+
+		},
+		getBindingParameters: function() {
+			return {};
+
+		},
+		_onButtonPress: function(oEvent) {
+
+			var oBindingContext = oEvent.getSource().getBindingContext();
+
+			return new Promise(function(fnResolve) {
+
+				this.doNavigate("ServiceDetailSearchSearch", oBindingContext, fnResolve, "");
+			}.bind(this)).catch(function(err) {
+				if (err !== undefined) {
+					MessageBox.error(err.message);
+				}
+			});
+
+		},
+		doNavigate: function(sRouteName, oBindingContext, fnPromiseResolve, sViaRelation) {
+			var sPath = (oBindingContext) ? oBindingContext.getPath() : null;
+			var oModel = (oBindingContext) ? oBindingContext.getModel() : null;
+
+			var sEntityNameSet;
+			if (sPath !== null && sPath !== "") {
+				if (sPath.substring(0, 1) === "/") {
+					sPath = sPath.substring(1);
+				}
+				sEntityNameSet = sPath.split("(")[0];
+			}
+			var sNavigationPropertyName;
+			var sMasterContext = this.sMasterContext ? this.sMasterContext : sPath;
+
+			if (sEntityNameSet !== null) {
+				sNavigationPropertyName = sViaRelation || this.getOwnerComponent().getNavigationPropertyForNavigationWithContext(sEntityNameSet, sRouteName);
+			}
+			if (sNavigationPropertyName !== null && sNavigationPropertyName !== undefined) {
+				if (sNavigationPropertyName === "") {
+					this.oRouter.navTo(sRouteName, {
+						context: sPath,
+						masterContext: sMasterContext
+					}, false);
+				} else {
+					oModel.createBindingContext(sNavigationPropertyName, oBindingContext, null, function(bindingContext) {
+						if (bindingContext) {
+							sPath = bindingContext.getPath();
+							if (sPath.substring(0, 1) === "/") {
+								sPath = sPath.substring(1);
+							}
+						} else {
+							sPath = "undefined";
+						}
+
+						// If the navigation is a 1-n, sPath would be "undefined" as this is not supported in Build
+						if (sPath === "undefined") {
+							this.oRouter.navTo(sRouteName);
+						} else {
+							this.oRouter.navTo(sRouteName, {
+								context: sPath,
+								masterContext: sMasterContext
+							}, false);
+						}
+					}.bind(this));
+				}
+			} else {
+				this.oRouter.navTo(sRouteName);
+			}
+
+			if (typeof fnPromiseResolve === "function") {
+				fnPromiseResolve();
+			}
+
+		},
+		_onButtonPress1: function() {
+
+			this.close();
+
+		},
+		onInit: function() {
+
+			this._oDialog = this.getControl();
+
+		},
+		onExit: function() {
+			this._oDialog.destroy();
+
+			// to destroy templates for bound aggregations when templateShareable is true on exit to prevent duplicateId issue
+			var aControls = [{
+				"controlId": "sap_m_Dialog_0-content-sap_m_ScrollContainer-1670309517575-content-build_simple_Table-1670309559486",
+				"groups": ["items"]
+			}, {
+				"controlId": "sap_m_Dialog_0-content-sap_m_VBox-1670308591528-items-build_simple_Table-1670309239976",
+				"groups": ["items"]
+			}];
+			for (var i = 0; i < aControls.length; i++) {
+				var oControl = this.getView().byId(aControls[i].controlId);
+				if (oControl) {
+					for (var j = 0; j < aControls[i].groups.length; j++) {
+						var sAggregationName = aControls[i].groups[j];
+						var oBindingInfo = oControl.getBindingInfo(sAggregationName);
+						if (oBindingInfo) {
+							var oTemplate = oBindingInfo.template;
+							oTemplate.destroy();
+						}
+					}
+				}
+			}
+
+		}
+
+	});
+}, /* bExport= */ true);
